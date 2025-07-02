ef3b715da233a7688c040f6c40c1b271d53abbda
Jonas-Esser <jonas.esser97@gmail.com>
2025-07-02 15:49:44 +0200

    Update code_main.R

--- code_main.R
+++ code_main.R
@@ -1,46 +1,42 @@
-library(ggplot2)
-library(gridExtra)
-
-
 data <- read.csv("c:\\Users\\jes238\\OneDrive - Vrije Universiteit Amsterdam\\Documents\\suBART_tutorial\\data_example.csv")
-X <- data[,c("t", "age", "sex", "education")]
+
+# data preparation ####
+X <- data[,c("t", "age", "gender", "education")]
 Y <- data[,c("c", "q")]
 
+# estimation of propensity scores ####
 library(subart)
-n_mcmc <- 2000
-ps_fit <- subart(x_train = X[,c("age", "sex", "education")],
+
+ps_fit <- subart(x_train = X[,c("age", "gender", "education")],
                  y_mat = as.matrix(X$t),
-                 x_test = X[,c("age", "sex", "education")],
+                 x_test = X[,c("age", "gender", "education")],
                  n_tree = 100,
-                 n_mcmc = 2*n_mcmc,
-                 n_burn = n_mcmc,
+                 n_mcmc = 3000,
+                 n_burn = 1000,
                  varimportance = TRUE)
 
 X_ps <- X
 X_ps$ps <- apply(pnorm(ps_fit$y_hat_test), 1, mean)
+
+# further data preparation ####
+
 X_test <- rbind(X_ps, X_ps)
 X_test$t <- c(rep(0, nrow(X)), rep(1, nrow(X)))
 
-# Fitting the suBART model
+# fitting the suBART model ####
 
 suBART_ps_fit <- subart(x_train = X_ps,
                         y_mat = as.matrix(Y),
                         x_test = X_test,
                         n_tree = 100,
-                        n_mcmc = 2*n_mcmc,
-                        n_burn = n_mcmc,
+                        n_mcmc = 3000,
+                        n_burn = 1000,
                         varimportance = FALSE)
 
+# obtaining the results ####
 
-post_suBART_ps <- data.frame(
-  Delta_c = suBART_ps_fit$ATE[,1],
-  Delta_q = suBART_ps_fit$ATE[,2]
-)
-
-# Postprocessing
-
-suBART_ps_fit$ATE <- matrix(NA, nrow = n_mcmc, ncol = 2)
-for (i in 1:n_mcmc){
+suBART_ps_fit$ATE <- matrix(NA, nrow = 2000, ncol = 2)
+for (i in 1:2000){
   suBART_ps_fit$ATE[i,] <- c(
     mean(suBART_ps_fit$y_hat_test[X_test$t == 1,1,i]) - mean(suBART_ps_fit$y_hat_test[X_test$t == 0,1,i]),
     mean(suBART_ps_fit$y_hat_test[X_test$t == 1,2,i]) - mean(suBART_ps_fit$y_hat_test[X_test$t == 0,2,i])
@@ -48,23 +44,25 @@ for (i in 1:n_mcmc){
   )
 }
 
-post_suBART_ps <- data.frame(
+MCMC_sample <- data.frame(
   Delta_c = suBART_ps_fit$ATE[,1],
   Delta_q = suBART_ps_fit$ATE[,2]
 )
-post_suBART_ps$INB20 <- 20000 * post_suBART_ps$Delta_q - post_suBART_ps$Delta_c
-post_suBART_ps$INB50 <- 50000 * post_suBART_ps$Delta_q - post_suBART_ps$Delta_c
+MCMC_sample$INB20 <- 20000 * MCMC_sample$Delta_q - MCMC_sample$Delta_c
+MCMC_sample$INB50 <- 50000 * MCMC_sample$Delta_q - MCMC_sample$Delta_c
 
 lambda <- seq(0, 50000, length.out = 1000)
 p <- rep(NA, length(lambda))
 for (i in 1:length(lambda)){
-  p[i] <- mean(lambda[i]*post_suBART_ps$Delta_q - post_suBART_ps$Delta_c > 0)
+  p[i] <- mean(lambda[i]*MCMC_sample$Delta_q - MCMC_sample$Delta_c > 0)
 }
 CEAC <- data.frame(lambda, p)
 
-# Plotting results
+# Plotting results ####
+library(ggplot2)
+library(gridExtra)
 
-CEplane <- ggplot(post_suBART_ps) + 
+CEplane <- ggplot(MCMC_sample) + 
   geom_point(mapping = aes(Delta_q, Delta_c), size = 1, alpha = 0.3, show.legend = FALSE) +
   geom_point(aes(mean(Delta_q), mean(Delta_c)), color = "red", size = 2) +
   geom_vline(xintercept = 0, linetype = "dashed") +
